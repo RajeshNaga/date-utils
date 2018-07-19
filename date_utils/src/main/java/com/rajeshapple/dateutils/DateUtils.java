fe35eebdc983c380ee1ccadb24abd6d3c893e242
Rajesh <rajesh@speedkyat.com>
2018-07-19 15:58:11 +0630

    first commit

---
+++ date_utils/src/main/java/com/rajeshapple/dateutils/DateUtils.java
@@ -0,0 +1,16 @@
+package com.rajeshapple.dateutils;
+
+import android.util.Log;
+
+/**
+ * Created by super on 7/19/18.
+ */
+
+public class DateUtils {
+
+
+    public void data_function(String msg)
+    {
+        Log.d(DateUtils.this.getClass().getSimpleName(), msg);
+    }
+}
